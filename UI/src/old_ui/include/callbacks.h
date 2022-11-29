c17c1e8304ccd61d3ab5fdf9882c611c67e8d1fd
Ender <ndxendernight@gmail.com>
2022-11-29 23:59:20 +0100

    Changed UI archi

---
+++ UI/src/old_ui/include/callbacks.h
@@ -0,0 +1,16 @@
+#ifndef CALLBACKS_H
+#define CALLBACKS_H
+
+#include "tools.h"
+
+void on_save_button_clicked(GtkButton *button, gpointer user_data);
+
+void on_open_button_clicked(GtkButton *button, gpointer user_data);
+
+void on_rotate_scale_value_changed(GtkRange *rotate, gpointer user_data);
+
+void on_solve_button_clicked(GtkButton *button, gpointer user_data);
+
+void on_resize(GtkWindow *window, gpointer user_data);
+
+#endif
