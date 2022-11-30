ba8532d4265c6d32bcf881d9a7f11b90b8158243
Spokloo <augustin.claude@epita.fr>
2022-12-01 00:48:39 +0100

    Auto rotation fixed and image 5 working

--- Grid_Detection/houghtransform.h
+++ Grid_Detection/houghtransform.h
@@ -18,6 +18,9 @@ typedef struct Square
     Point p1, p2, p3, p4;
 } Square;
 
-void hough_transform(Image *img);
+void hough_transform(Image *img, unsigned int *lines_len, int ***real_lines);
+void auto_rotation(Image *img, Image *copy_img, unsigned int *lines_len, int ***real_lines);
+void squares(Image *img, unsigned int *lines_len, int ***real_lines, Square **gs);
+void perspective(Image *img, Square **gs);
 
 #endif
--- Grid_Detection/main.c
+++ Grid_Detection/main.c
@@ -11,9 +11,20 @@ int main(int argc, char **argv)
         errx(1, "Usage: ./main <path to image>");
 
     Image img = load_image(argv[1]);
+    Image copy_img;
+    copy_image(&img, &copy_img);
+
+    int **real_lines = NULL;
+    unsigned int lines_len = 0;
+    Square *gs = NULL;
 
     time_t start = time(NULL);
-    hough_transform(&img);
+    hough_transform(&img, &lines_len, &real_lines);
+    save_image(&img, "hough_lines.jpeg");
+
+    auto_rotation(&img, &copy_img, &lines_len, &real_lines);
+    squares(&img, &lines_len, &real_lines, &gs);
+    perspective(&img, &gs);
     time_t end = time(NULL);
 
     printf("Execution took %lds.\n", (unsigned long) difftime(end, start));
