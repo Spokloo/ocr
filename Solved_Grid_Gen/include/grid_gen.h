abf669a74cc14044b89bfcf2752a141ca903b537
Ender <ndxendernight@gmail.com>
2022-12-06 02:31:37 +0100

    [Grid Gen] rename place_image -> place_img

--- Solved_Grid_Gen/include/grid_gen.h
+++ Solved_Grid_Gen/include/grid_gen.h
@@ -7,7 +7,7 @@
 #include "image.h"
 #include "solver_fun.h"
 
-void place_image(Image *dest, int digit, int x, int y);
+void place_img(Image *dest, Pixel *color, int digit, int x, int y);
 void printHelp();
 void construct_grid(char *grid_path, char *blank_grid_path, char *save_path);
 
